3cf99cd3f732372bfe41dc1b20d09d542985e22e
farreladriann <muhamadfarreladrian@mail.ugm.ac.id>
2024-11-26 04:13:37 +0700

    conditionally create logs directory and modify logger stream for development environment

--- src/config/morganLogger.ts
+++ src/config/morganLogger.ts
@@ -6,10 +6,15 @@ import moment from 'moment-timezone';
 
 const logsDir = path.resolve(__dirname, '../logs');
 const accessLogPath = path.join(logsDir, 'access.log');
-
 let accessLogStream: fs.WriteStream | undefined;
-if (process.env.NODE_ENV === 'development')
+
+// Buat directory logs hanya di development
+if (process.env.NODE_ENV === 'development') {
+  if (!fs.existsSync(logsDir)) {
+    fs.mkdirSync(logsDir, { recursive: true });
+  }
   accessLogStream = fs.createWriteStream(accessLogPath, { flags: 'a' });
+}
 
 morgan.token('date', () =>
   moment().tz('Asia/Jakarta').format('YYYY-MM-DD HH:mm:ss'),
@@ -30,9 +35,14 @@ const customLoggerFormat =
   ':remote-addr - :remote-user [:date] ":method :url HTTP/:http-version" ' +
   ':status :res[content-length] ":referrer" ":user-agent" "Origin: :origin" "CORS Status: :isCrossOrigin"';
 
+// Modifikasi loggerStream untuk handle both environments
 const loggerStream: StreamOptions = {
   write: (message) => {
-    accessLogStream!.write(message);
+    if (process.env.NODE_ENV === 'development' && accessLogStream) {
+      accessLogStream.write(message);
+    } else {
+      console.log(message.trim());
+    }
   },
 };
 
